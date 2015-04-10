1605fb329048258aeae4d20117d2562618ace1ff
unknown <jopss@ideebox.com.br>
2015-04-09 21:16:14 -0300

    corrigindo parse XML

--- src/main/java/br/com/jopss/pagseguro/assinaturas/modelos/suporte/PreAprovacao.java
+++ src/main/java/br/com/jopss/pagseguro/assinaturas/modelos/suporte/PreAprovacao.java
@@ -11,6 +11,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 /**
@@ -22,6 +23,7 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 @XmlRootElement(name = "preApproval")
 public class PreAprovacao {
 	
+        @XmlTransient
 	private final DecimalFormat fmt = new DecimalFormat("##0.00", new DecimalFormatSymbols(Locale.US));
 	
 	@XmlElement(name = "charge")
